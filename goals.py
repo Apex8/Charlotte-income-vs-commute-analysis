812952ee33888a1bf870412c6eeefcf093cbcc90
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-07-25 12:12:01 -0400

    Update goals.py

--- goals.py
+++ goals.py
@@ -1,7 +1,8 @@
 # Pull in API data from Charlotte api website on commuting block groups and income block groups
-# Block Group with highest median income
-# Block group with largest number under 10000k yearly income
-# Block group with largest number over 200000k yearly income
+# Total Households per block group
+# Block Group with highest median income and location
+# Block group with largest number under 10000k yearly income and location
+# Block group with largest number over 200000k yearly income and location
 # Block group with largest number of commuters of each of the following categories:
 # Drive Alone
 # Carpool
