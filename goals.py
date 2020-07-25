abc387f071d01c7dc12a6e42a30ba33b68915399
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-07-25 12:56:00 -0400

    Update goals.py

--- goals.py
+++ goals.py
@@ -1,4 +1,7 @@
 # Pull in API data from Charlotte api website on commuting block groups and income block groups
+# Merge on GEOID10 
+# in household income block groups keeping total households, 0-50K, 50-100, 100-150, 150-200+, Median household income, total families, families in poverty
+# in commuting block groups keeping workers 16 or older, commuters drive alone, commuters carpool, commuters public transportation, commuters walked, commuters other means
 # Total Households per block group
 # Block Group with highest median income and location
 # Block group with largest number under 10000k yearly income and location
