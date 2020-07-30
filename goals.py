90dc5073ab5fd818557e9e9985632c1544f7d866
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-07-30 18:57:09 -0400

    figure update

--- goals.py
+++ goals.py
@@ -2,7 +2,8 @@
 # Merge on GEOID10 
 # from household income block groups keeping total households, merging smaller groups to 50k intervals( 0-50K, 50-100, 100-150, 150-200+), Median household income, total families, families in poverty.
 # from commuting block groups keeping workers 16 or older, commuters drive alone, commuters carpool, commuters public transportation, commuters walked, commuters other means.
-# Block group with largest number of commuters of each of the following categories and their median income statistic: Drive Alone, Carpool, Public Transport, Walked, Other means.
-# Block with the largest number of households per each 50k (0-50, 50-100, 100-150, 150-200).
+# Plotting the total amount per income bracket (Fig. 1) and the total amount per commuting style in the city of Charlotte (Fig. 2). 
+# Block group with largest number of commuters of each of the following categories and their median income statistic: Drive Alone (Fig. 3), Carpool(Fig. 4), Public Transport(Fig. 5), Walked(Fig. 6), Other means(Fig. 7).
+# Block with the largest number of households per each 50k (0-50, 50-100, 100-150, 150-200)(Fig. 8).
 # Commuters per category and that average income for every block to determine most used transport per income bracket.
 
